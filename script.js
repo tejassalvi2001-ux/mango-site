27e0e960b4f21e07fe712ed17e699b04782e538e
Tejas Salvi <tejassalvi2001@gmail.com>
2025-11-27 12:35:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// Navbar Hide on Scroll
+let lastScroll = 0;
+const navbar = document.getElementById("mainNavbar");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll) {
+    navbar.style.top = "-80px"; // Hide
+  } else {
+    navbar.style.top = "0"; // Show
+  }
+  lastScroll = currentScroll;
+});
+
+
+// Popup Functions
+function openPopup() {
+  document.getElementById("contactPopup")..style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("contactPopup").style.display = "none";
+}
